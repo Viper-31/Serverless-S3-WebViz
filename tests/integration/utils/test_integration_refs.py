f32cdfae2f4ea3fb18f4f9568fdac17c625fff43
JohnC <chewyunpin@gmail.com>
2026-05-23 18:03:13 +0800

    mark integration tests

--- tests/integration/utils/test_integration_refs.py
+++ tests/integration/utils/test_integration_refs.py
@@ -8,7 +8,7 @@ from utils.config_utils import load_pipeline_config
 from utils.ledger_utils import filter_ledger_keys_by_flow_id, load_inventory_ledger
 from utils.ref_loader import ref_path_for_ledger_key
 
-
+@pytest.mark.integration
 def test_integration_ecmwf_ref_paths_exist():
     cfg = load_pipeline_config("utils/config.yaml")
     ledger = load_inventory_ledger(cfg["paths"]["ledger_path"])
@@ -20,7 +20,7 @@ def test_integration_ecmwf_ref_paths_exist():
     ref_path = ref_path_for_ledger_key(cfg, keys[0])
     assert Path(ref_path).exists()
 
-
+@pytest.mark.integration
 def test_integration_dpird_ref_paths_exist():
     cfg = load_pipeline_config("utils/config.yaml")
     ledger = load_inventory_ledger(cfg["paths"]["ledger_path"])
