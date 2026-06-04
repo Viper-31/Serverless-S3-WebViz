073612c9586db470a52d6080464517f0956a23c8
JohnC <chewyunpin@gmail.com>
2026-06-04 17:34:25 +0800

    mv decodeBase64 to domain/dpird/

--- web-app/src/map/controllers/__tests__/mapController.test.ts
+++ web-app/src/map/controllers/__tests__/mapController.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import {
   createDpirdMapController,
   createEcmwfMapController,
-} from "../../src/lib/mapController";
+} from "../mapController";
 
 function createFakeMap() {
   const layers = new Set<string>();
