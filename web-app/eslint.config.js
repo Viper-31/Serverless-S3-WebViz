0c5cfe31ffea70a5a4bcc9e06831c2f3d375bab5
JohnC <chewyunpin@gmail.com>
2026-07-09 17:51:10 +0800

    chore: Add lint shortcuts to package.json
    Extend rule for svelte eslint and ignore build files

--- web-app/eslint.config.js
+++ web-app/eslint.config.js
@@ -8,6 +8,7 @@ import svelteConfig from "./svelte.config.js";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["node_modules/**", "dist/**", "build/**", "coverage/**"] },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
@@ -15,15 +16,30 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  svelte.configs.recommended,
+
+  ...svelte.configs.recommended.map((config) => ({
+    ...config,
+    files: config.files ?? ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+  })),
   {
     files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
     languageOptions: {
       parserOptions: {
         projectService: true,
         extraFileExtensions: [".svelte"],
+        parser: tseslint.parser,
         svelteConfig,
       },
+    },
+  },
+  {
+    files: [
+      "**/*.test.{ts,js}",
+      "**/__tests__/**/*.{ts,js}",
+      "test/**/*.{ts,js}",
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
   {
